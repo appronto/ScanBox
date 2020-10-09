1e0ae89866328794a3e21030ff729d32c479b68f
Joël vd Graaf <jvdgraaf36@gmail.com>
2020-10-09 15:13:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Scanbutton
+
+A barcode scanning device for a pc or laptop is just an input device like a keyboard, it's just very fast with typing.
+This widget is a simple inputbox which automatically sets the focus on the input and tracks when an user starts typing, when he stops it triggers an onchange microflow or nanoflow.
+
+## Typical usage scenario
+
+- Execute an onchange microflow or nanoflow after the user stops typing
+- Barcode scanner device
+
+## Features and limitations
+
+- Auto trigger microflow and/or nanoflow after the users stops typing 
+- Handling of validation messages
+
+## Installation
+
+- Just download the widget from the appstore!
+
+## Configuration
+
+- Place the widget in a dataview or listview.
+- Fill in the ìnput attribute for the textbox.
+- Set an onchange microflow or nanoflow
---
+++ src/ScanBox/widget/ScanBox.js
@@ -0,0 +1,290 @@
+/*global logger*/
+/*
+    ScanBox
+    ========================
+
+    @file      : ScanBox.js
+    @version   : 1.0.0
+    @author    : J vd Graaf
+    @date      : Fri, 09 Oct 2020 09:30:19 GMT
+    @copyright : Appronto
+    @license   : Apache V2
+
+    Documentation
+    ========================
+    Describe your widget here.
+*/
+
+// Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
+define([
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+    "dijit/_TemplatedMixin",
+
+    "mxui/dom",
+    "dojo/dom",
+    "dojo/dom-prop",
+    "dojo/dom-geometry",
+    "dojo/dom-class",
+    "dojo/dom-style",
+    "dojo/dom-construct",
+    "dojo/_base/array",
+    "dojo/_base/lang",
+    "dojo/text",
+    "dojo/html",
+    "dojo/_base/event",
+
+    "ScanBox/lib/jquery-1.11.2",
+    "dojo/text!ScanBox/widget/template/ScanBox.html"
+], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent, _jQuery, widgetTemplate) {
+    "use strict";
+
+    var $ = _jQuery.noConflict(true);
+
+    // Declare widget's prototype.
+    return declare("ScanBox.widget.ScanBox", [_WidgetBase, _TemplatedMixin], {
+        // _TemplatedMixin will create our dom node using this HTML template.
+        templateString: widgetTemplate,
+
+        // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
+        _handles: null,
+        _contextObj: null,
+        _alertDiv: null,
+        _readOnly: false,
+
+        // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
+        constructor: function () {
+            logger.debug(this.id + ".constructor");
+            this._handles = [];
+        },
+
+        // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
+        postCreate: function () {
+            logger.debug(this.id + ".postCreate");
+
+            if (this.readOnly || this.get("disabled") || this.readonly) {
+                this._readOnly = true;
+            }
+
+            this._updateRendering();
+            this._setupEvents();
+        },
+
+        // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
+        update: function (obj, callback) {
+            logger.debug(this.id + ".update");
+
+            this._contextObj = obj;
+            this._resetSubscriptions();
+            this._updateRendering(callback); // We're passing the callback to updateRendering to be called after DOM-manipulation
+        },
+
+        // mxui.widget._WidgetBase.enable is called when the widget should enable editing. Implement to enable editing if widget is input widget.
+        enable: function () {
+            logger.debug(this.id + ".enable");
+        },
+
+        // mxui.widget._WidgetBase.enable is called when the widget should disable editing. Implement to disable editing if widget is input widget.
+        disable: function () {
+            logger.debug(this.id + ".disable");
+        },
+
+        // mxui.widget._WidgetBase.resize is called when the page's layout is recalculated. Implement to do sizing calculations. Prefer using CSS instead.
+        resize: function (box) {
+            logger.debug(this.id + ".resize");
+            this.scaninputbox.focus();
+        },
+
+        // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
+        uninitialize: function () {
+            logger.debug(this.id + ".uninitialize");
+            // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
+        },
+
+        // We want to stop events on a mobile device
+        _stopBubblingEventOnMobile: function (e) {
+            logger.debug(this.id + "._stopBubblingEventOnMobile");
+            if (typeof document.ontouchstart !== "undefined") {
+                dojoEvent.stop(e);
+            }
+        },
+
+        // Attach events to HTML dom elements
+        _setupEvents: function () {
+            logger.debug(this.id + "._setupEvents");
+
+            //setup before functions
+            var typingTimer; //timer identifier
+
+            //on keyup, start the countdown
+            this.scaninputbox.onkeyup = lang.hitch(this, function () {
+                clearTimeout(typingTimer);
+                typingTimer = setTimeout(lang.hitch(this, function () {
+                    this._processValue(this.scaninputbox.value);
+                }), this.timeout);
+            });
+
+            //on keydown, clear the countdown 
+            this.scaninputbox.onkeydown = lang.hitch(this, function () {
+                clearTimeout(typingTimer);
+            });
+        },
+
+        _processValue: function (code) {
+            this._contextObj.set(this.scanAttr, code);
+            if (this.onChangeNF) {
+                this._exeNf(this.onChangeNF, this._contextObj.getGuid(), lang.hitch(this, this._processValueCB));
+            }
+            if (this.onChangeMF) {
+                this._exeMf(this.onChangeMF, this._contextObj.getGuid(), lang.hitch(this, this._processValueCB));
+            }
+        },
+        
+        _processValueCB: function(result){
+            this.scaninputbox.focus();
+        },
+
+        _exeNf: function (nf, guid, cb) {
+            logger.debug(this.id + "._execNf");
+            mx.data.callNanoflow({
+                nanoflow: nf,
+                origin: this.mxform,
+                context: this.mxcontext,
+                callback: lang.hitch(this, function (objs) {
+                    if (cb && typeof cb === "function") {
+                        cb(objs);
+                    }
+                }),
+                error: function (error) {
+                    alert(error.message);
+                }
+            });
+        },
+
+        _execMf: function (mf, guid, cb) {
+            logger.debug(this.id + "._execMf");
+            if (mf && guid) {
+                mx.ui.action(mf, {
+                    params: {
+                        applyto: "selection",
+                        guids: [guid]
+                    },
+                    callback: lang.hitch(this, function (objs) {
+                        if (cb && typeof cb === "function") {
+                            cb(objs);
+                        }
+                    }),
+                    error: function (error) {
+                        console.debug(error.description);
+                    }
+                }, this);
+            }
+        },
+
+        // Rerender the interface.
+        _updateRendering: function (callback) {
+            logger.debug(this.id + "._updateRendering");
+            
+            if(this._contextObj){
+                this.scaninputbox.value = this._contextObj.get(this.scanAttr);
+            }
+            
+            if (this._readOnly) {
+                this.scaninputbox.disabled = true;
+            } else {
+                this.scaninputbox.disabled = false;
+                this.scaninputbox.focus();
+                logger.debug(this.id + "._updateRendering set focus");
+            }
+
+            // Important to clear all validations!
+            this._clearValidations();
+
+            // The callback, coming from update, needs to be executed, to let the page know it finished rendering
+            this._executeCallback(callback, "_updateRendering");
+        },
+
+        // Handle validations.
+        _handleValidation: function (validations) {
+            logger.debug(this.id + "._handleValidation");
+            this._clearValidations();
+
+            var validation = validations[0],
+                message = validation.getReasonByAttribute(this.scanAttr);
+
+            if (this._readOnly) {
+                validation.removeAttribute(this.scanAttr);
+            } else if (message) {
+                this._addValidation(message);
+                validation.removeAttribute(this.scanAttr);
+            }
+        },
+
+        // Clear validations.
+        _clearValidations: function () {
+            logger.debug(this.id + "._clearValidations");
+            dojoConstruct.destroy(this._alertDiv);
+            this._alertDiv = null;
+        },
+
+        // Show an error message.
+        _showError: function (message) {
+            logger.debug(this.id + "._showError");
+            if (this._alertDiv !== null) {
+                dojoHtml.set(this._alertDiv, message);
+                return true;
+            }
+            this._alertDiv = dojoConstruct.create("div", {
+                "class": "alert alert-danger",
+                "innerHTML": message
+            });
+            dojoConstruct.place(this._alertDiv, this.domNode);
+        },
+
+        // Add a validation.
+        _addValidation: function (message) {
+            logger.debug(this.id + "._addValidation");
+            this._showError(message);
+        },
+
+        // Reset subscriptions.
+        _resetSubscriptions: function () {
+            logger.debug(this.id + "._resetSubscriptions");
+            // Release handles on previous object, if any.
+            this.unsubscribeAll();
+
+            // When a mendix object exists create subscribtions.
+            if (this._contextObj) {
+                this.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    callback: lang.hitch(this, function (guid) {
+                        this._updateRendering();
+                    })
+                });
+
+                this.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    attr: this.scanAttr,
+                    callback: lang.hitch(this, function (guid, attr, attrValue) {
+                        this._updateRendering();
+                    })
+                });
+
+                this.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    val: true,
+                    callback: lang.hitch(this, this._handleValidation)
+                });
+            }
+        },
+
+        _executeCallback: function (cb, from) {
+            logger.debug(this.id + "._executeCallback" + (from ? " from " + from : ""));
+            if (cb && typeof cb === "function") {
+                cb();
+            }
+        }
+    });
+});
+
+require(["ScanBox/widget/ScanBox"]);
